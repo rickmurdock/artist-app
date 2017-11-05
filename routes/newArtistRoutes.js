a80518741c73846fa531b6cecd8a7db8d4b7b116
rickmurdock <rick.murdock@live.com>
2017-11-05 14:51:21 -0600

    Changes to keep dups from being inserted.

--- routes/newArtistRoutes.js
+++ routes/newArtistRoutes.js
@@ -12,17 +12,45 @@ newArtistRouter.post('/', (req, res) => {
   console.log(req.body);
 
   let artistData = req.body;
-  let newArtist = new Artist(artistData);
-  //newArtist.albums.push({albumName: artistData.albumName})
-  newArtist.albums.push(artistData.albumName)
-  newArtist
-    .save()
+  console.log('Arist Name is ', artistData.artistName);
+
+//
+  let query = artistData.artistName
+  
+  Artist
+    .findOneAndUpdate({ artistName: artistData.artistName }, { artistName: artistData.artistName, $addToSet: { albums: artistData.albumName }}, { upsert: true } )
     .then(savedArtist => {
+
+      // if(artistData.albumName != ''){
+      //   console.log('ALBUM ENTERED', artistData.albumName );
+      //   Artist.update(
+      //     { artistName: artistData.artistName },
+      //     { $addToSet: { albums: artistData.albumName }}
+      //   )
+
+
+      // } else{
+      //   console.log('ALUBUM BLANK----');
+      // }
+        
+  
+
       res.redirect('/artist');
     })
     .catch(err => {
       res.status(500).send(err);
     });
+
+  // let newArtist = new Artist(artistData);
+  // newArtist.albums.push(artistData.albumName)
+  // newArtist
+  //   .save()
+  //   .then(savedArtist => {
+  //     res.redirect('/artist');
+  //   })
+  //   .catch(err => {
+  //     res.status(500).send(err);
+  //   });
 
 });
 
